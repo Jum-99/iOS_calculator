7eda360c18272f4fb53cbae5e79394b236824bcc
Jum-99 <jakkcer.fivework@gmail.com>
2017-12-29 22:57:36 +0900

    Set status bar

--- iOS Calculator/iOS Calculator/ViewController.swift
+++ iOS Calculator/iOS Calculator/ViewController.swift
@@ -25,10 +25,11 @@ class ViewController: UIViewController {
         
         // constant setting
         _size = UIScreen.main.bounds.size;
-        self.view.backgroundColor = .white;
+        self.view.backgroundColor = .black;
         
         // view setting
-        setMainView();
+//        setMainView();
+        setStatusBar();
         
         print("Did load: CalculatorViewController");
     }
@@ -42,10 +43,16 @@ class ViewController: UIViewController {
     // View Setting
     //------------------------------------------
     
-    func setMainView() {
-        _mainView = UIView(frame: CGRect(origin: CGPoint.zero, size: _size));
-        _mainView.backgroundColor = .blue;
-        self.view.addSubview(_mainView);
+//    func setMainView() {
+//        _mainView = UIView(frame: CGRect(origin: CGPoint.zero, size: _size));
+//        _mainView.backgroundColor = .blue;
+//        self.view.addSubview(_mainView);
+//    }
+    
+    func setStatusBar() {
+        let statusBar = UIView(frame: CGRect(x: 0, y: 0, width: _size.width, height: 20));
+        statusBar.backgroundColor = UIColor(red: 0.13, green: 0.13, blue: 0.13, alpha: 1.0);
+        self.view.addSubview(statusBar);
     }
     
 
